1fddeca1ac0f350b564568bc9835aae88fd0e616
weiyk <weiyk@tingyun.com>
2020-03-21 00:55:53 +0800

    修复类重复定义的问题

--- jtrace-asm/src/main/java/com/github/wei/jtrace/asm/TransformerMatcherWrapper.java
+++ jtrace-asm/src/main/java/com/github/wei/jtrace/asm/TransformerMatcherWrapper.java
@@ -16,6 +16,8 @@ import java.security.ProtectionDomain;
 import java.util.List;
 
 public class TransformerMatcherWrapper implements ITransformerMatcher, ITransformer {
+    private final String JAVA_OBJECT_TYPE = "java/lang/Object";
+
     private IMethodTransformerMatcher methodTransformerMatcher;
 
     public TransformerMatcherWrapper(IMethodTransformerMatcher methodTransformerMatcher){
@@ -36,7 +38,7 @@ public class TransformerMatcherWrapper implements ITransformerMatcher, ITransfor
     }
 
     @Override
-    public byte[] transform(ClassLoader loader, IClassDescriberTree descr,
+    public byte[] transform(final ClassLoader loader, IClassDescriberTree descr,
                             Class<?> classBeingRedefined,
                             ProtectionDomain protectionDomain,
                             byte[] classfileBuffer) throws IllegalClassFormatException {
@@ -62,7 +64,42 @@ public class TransformerMatcherWrapper implements ITransformerMatcher, ITransfor
             return null;
         }
 
-        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
+        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS) {
+
+            /**
+             * 因为ClassLoader隔离，所以在ASM所在的ClassLoader中找不到应用的类，
+             * 真正的ClassLoader应该是transform传递进来的ClassLoader
+             */
+            @Override
+            protected String getCommonSuperClass(String type1, String type2) {
+                if(JAVA_OBJECT_TYPE.equals(type1) || JAVA_OBJECT_TYPE.equals(type2)){
+                    return JAVA_OBJECT_TYPE;
+                }
+
+                Class<?> c, d;
+                try {
+
+                    c = Class.forName(type1.replace('/', '.'), false, loader);
+                    d = Class.forName(type2.replace('/', '.'), false, loader);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+                if (c.isAssignableFrom(d)) {
+                    return type1;
+                }
+                if (d.isAssignableFrom(c)) {
+                    return type2;
+                }
+                if (c.isInterface() || d.isInterface()) {
+                    return JAVA_OBJECT_TYPE;
+                } else {
+                    do {
+                        c = c.getSuperclass();
+                    } while (!c.isAssignableFrom(d));
+                    return c.getName().replace('.', '/');
+                }
+            }
+        };
         classNode.accept(cw);
         return cw.toByteArray();
     }
